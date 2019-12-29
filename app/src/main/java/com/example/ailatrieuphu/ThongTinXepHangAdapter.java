070ac544ad1b287371fd97fc27930bcfe4110532
CapooCat <54877892+CapooCat@users.noreply.github.com>
2019-12-29 11:10:52 +0700

    update XepHang

--- app/src/main/java/com/example/ailatrieuphu/ThongTinXepHangAdapter.java
+++ app/src/main/java/com/example/ailatrieuphu/ThongTinXepHangAdapter.java
@@ -1,13 +1,16 @@
 package com.example.ailatrieuphu;
 
 import android.content.Context;
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 
@@ -19,6 +22,7 @@ public class ThongTinXepHangAdapter extends RecyclerView.Adapter<RecyclerView.Vi
     private final static int TYPE_VIEW_LOADING = 1;
     private final ArrayList<ThongTinXepHang> mListThongTinXepHang;
     private final LayoutInflater mInflater;
+
 
     public ThongTinXepHangAdapter(Context context, ArrayList<ThongTinXepHang> mListThongTinXepHang) {
         this.mListThongTinXepHang = mListThongTinXepHang;
@@ -60,6 +64,7 @@ public class ThongTinXepHangAdapter extends RecyclerView.Adapter<RecyclerView.Vi
     class ThongTinXepHangViewHolder extends  RecyclerView.ViewHolder {
         private final TextView tenTaiKhoan;
         private final TextView diemCaoNhat;
+        private final TextView Ranking;
         private final ThongTinXepHangAdapter mAdapter;
 
         public ThongTinXepHangViewHolder(@NonNull View itemView, ThongTinXepHangAdapter mAdapter) {
@@ -67,6 +72,7 @@ public class ThongTinXepHangAdapter extends RecyclerView.Adapter<RecyclerView.Vi
             this.mAdapter = mAdapter;
             this.tenTaiKhoan = itemView.findViewById(R.id.lblTenTaiKhoan);
             this.diemCaoNhat = itemView.findViewById(R.id.lblDiem);
+            this.Ranking = itemView.findViewById(R.id.profile7);
         }
     }
 
@@ -80,9 +86,22 @@ public class ThongTinXepHangAdapter extends RecyclerView.Adapter<RecyclerView.Vi
     }
 
     void hienThiThongTin(ThongTinXepHangViewHolder holder, int position) {
+
+        int Rank = position + 1;
+        if(Rank == 1) {
+            holder.Ranking.setBackgroundResource(R.drawable.buttonstyle6);
+        }
+        else if(Rank == 2)
+            holder.Ranking.setBackgroundResource(R.drawable.buttonstyle7);
+        else if(Rank == 3)
+            holder.Ranking.setBackgroundResource(R.drawable.buttonstyle8);
+        else
+            holder.Ranking.setBackgroundResource(R.drawable.buttonstyle2);
         ThongTinXepHang nguoiChoi = this.mListThongTinXepHang.get(position);
         holder.tenTaiKhoan.setText(nguoiChoi.getTenTaiKhoan());
         holder.diemCaoNhat.setText(Integer.toString(nguoiChoi.getDiemCaoNhat()));
+        holder.Ranking.setText(Integer.toString(Rank));
+
     }
 
     void hienThiProgressBar(LoadingViewHolder holder) {
